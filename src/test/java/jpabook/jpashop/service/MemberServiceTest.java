bf47eb9551a2e4bc9ccb99607f733371794f8c71
yoongyo <jyg0172@naver.com>
2021-08-16 00:24:16 +0900

    test application.yml

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,21 +17,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
     @Test
-    @Rollback(false)
     public void 회원가입() throws Exception {
+        // given
         Member member = new Member();
         member.setName("kim");
 
+        // when
         Long savedId = memberService.join(member);
 
+        // then
         assertEquals(member, memberRepository.findOnd(savedId));
     }
 
 
     @Test
     public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
 
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            return;
+        }
+        memberService.join(member2);   // 예외가 발생해야 한다.
+
+        // then
+        fail("예외가 발생해야 한다. ");
     }
 }
