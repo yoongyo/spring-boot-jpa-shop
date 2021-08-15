7eef9f1475aeec9a83f30e1211ea8a9bb699bc7e
yoongyo <jyg0172@naver.com>
2021-08-16 01:52:45 +0900

    test order service

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -46,8 +46,19 @@ public class OrderService {
         return order.getId();
     }
 
-    // 취소
-    
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+
+        //
+    }
 
 
     // 검색
