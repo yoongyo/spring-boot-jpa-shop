ffebd940e9b1269c6680028bc7f5fee8716e6e0d
yoongyo <jyg0172@naver.com>
2021-08-15 19:29:54 +0900

    jpashop

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name ="order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @Enumerated(EnumType.STRING)
+    private LocalDateTime orderDate;
+
+    private OrderStatus status;
+
+}
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -14,6 +15,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception {
         // given
         Member member = new Member();
@@ -26,5 +28,6 @@ class MemberRepositoryTest {
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
