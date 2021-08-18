dc6ca4ddadefa28214da9571aa8da11ced2bd8ab
yoongyo <jyg0172@naver.com>
2021-08-18 16:22:40 +0900

    api 성능최적화

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -26,28 +29,62 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-//    @GetMapping("/api/v1/simple-orders")
-//    public List<Order> ordersV1() {
-//
-//    }
-//    @GetMapping("/api/v2/simple-orders")
-//    public List<SimpleOrderDto> ordersV2() {
-//        orderRepository.fina
-//    }
-//
-//    @Data
-//    static class SimpleOrderDto {
-//        private Long orderId;
-//        private String name;
-//        private LocalDateTime orderDate;
-//        private OrderStatus orderStatus;
-//        private Address address;
-//
-//        public SimpleOrderDto(Order order) {
-//            orderId = order.getId();
-//            order.getMember().getName();
-//        }
-//    }
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // DTO 로 변환해 반환했지만...
+        // 1 + N + N 문제 발생(성능 문제)
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        // query 를 다 가져온다. (select 절에서 쓸모 없는 것 까지)
+        // 그렇다고 성능 차이가 많이 나진 않는다.
+        // 재사용성이 좋다.
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        // query 를 원하는 것만 가져온다.
+        // 단 재사용성이 떨어짐.
+        // 고로 트레이드오프.
+        return orderRepository.findOrderDtos();
+    }
 
 
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();  // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();  // LAZY 초기화
+        }
+    }
 }
